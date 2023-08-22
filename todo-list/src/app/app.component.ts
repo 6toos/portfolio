a9b9229d7da60a0b185014464c18ea46b99685a6
6toos <chukofsi@gmail.com>
2023-08-22 22:10:29 +0100

    another commit for the month

---
+++ todo-list/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-list';
+  tasks = [
+    "Visit Ann",
+    "Call Dad",
+    "Go to the gym",
+    "Wash the dishes",
+    "Shop for the party"
+  ]
+  add(note: string) {
+    this.tasks.push(note);
+  }
+}
